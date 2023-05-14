67f8cd656b90146fc3da8a62348b045a3a57c735
Adithya V <adithyavinod1943@gmail.com>
2023-05-14 11:04:11 +0530

    initialized google auth

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+
+app.set("view engine","ejs")
+app.get("/",(req,res)=>{
+    res.render("./pages/index")
+})
+
+app.listen(5000,()=>{
+    console.log("Server Started")
+})
