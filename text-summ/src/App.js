8ac3b4c2efadc60401b1e2fd629570a074c2d774
Adithya V <adithyavinod1943@gmail.com>
2023-05-22 01:23:27 +0530

    Version 0.1 waiting for deployment and styling

--- text-summ/src/App.js
+++ text-summ/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import React,{useState} from 'react';
 import axios from 'axios';
 const config = require("./config.json")
+
 function App() {
   const [text,setText]  = useState("")
+  const [stext,setSText] = useState("")
   const handleClick = async()=>{
     const response = await axios.post("https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
     {inputs:text},
@@ -15,16 +17,23 @@ function App() {
     }
     )
     console.log(response.data)
+    setSText(response.data[0]["summary_text"])
   }
-  const handleBtn = ()=>{
-    alert(`${process.env.REACT_APP_URL}`)
-  }
+  
   return (
     <div className="App">
+      <h2 className='title'>Text Summariser</h2>
       <textarea className="text-area" value={text} onChange={(e)=>setText(e.target.value)} rows={10}
       />
       <button className="btn" onClick={handleClick}>Summarise</button>
-      <button className='btn2' onClick={handleBtn}>Show</button>
+      <div>
+        <h3 className='summary-title'>Summary</h3>
+        <div className='summary'>
+          <p>
+          {stext}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
