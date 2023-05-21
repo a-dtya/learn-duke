76658264fedcf27be4482c8678e0d1ed2f9705cd
Adithya V <adithyavinod1943@gmail.com>
2023-05-22 01:06:03 +0530

    First testing passed

--- text-summ/src/App.js
+++ text-summ/src/App.js
@@ -2,25 +2,29 @@
 import './App.css';
 import React,{useState} from 'react';
 import axios from 'axios';
-
+const config = require("./config.json")
 function App() {
   const [text,setText]  = useState("")
   const handleClick = async()=>{
-    const response = await axios.get("https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+    const response = await axios.post("https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
     {inputs:text},
     {
       headers:{
-        Authorization: `Bearer ${process.env.REACT_APP_URL}`
+        Authorization: `Bearer ${config.url}`
       }
     }
     )
     console.log(response.data)
+  }
+  const handleBtn = ()=>{
+    alert(`${process.env.REACT_APP_URL}`)
   }
   return (
     <div className="App">
       <textarea className="text-area" value={text} onChange={(e)=>setText(e.target.value)} rows={10}
       />
       <button className="btn" onClick={handleClick}>Summarise</button>
+      <button className='btn2' onClick={handleBtn}>Show</button>
     </div>
   );
 }
