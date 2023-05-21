feb8a4c8fb61078ff9784ee44b6dbb2610b760f8
Adithya V <adithyavinod1943@gmail.com>
2023-05-21 21:22:54 +0530

    Adding API endPoint

--- text-summ/src/App.js
+++ text-summ/src/App.js
@@ -1,12 +1,24 @@
 
 import './App.css';
 import React,{useState} from 'react';
+import axios from 'axios';
 function App() {
   const [text,setText]  = useState("")
+  const handleClick = async()=>{
+    const response = await axios.get("https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
+    {inputs:text},
+    {
+      headers:{
+        Authorization: `Bearer ${process.env.AUTH}`
+      }
+    }
+    )
+  }
   return (
     <div className="App">
       <textarea className="text-area" value={text} onChange={(e)=>setText(e.target.value)} rows={10}
       />
+      <button className="btn" onClick={handleClick}>Summarise</button>
     </div>
   );
 }
